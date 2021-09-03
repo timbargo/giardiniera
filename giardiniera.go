bd998f7fd028660f193e6c181fa8fbac96664c75
tim <tim@timbargo.com>
2021-09-03 18:08:09 -0500

    Add starter code

--- giardiniera.go
+++ giardiniera.go
@@ -2,20 +2,6 @@ package main
 
 import "fmt"
 
-// So this is a Pokemon knockoff and because I know so much Pokemon it will have mechanics that work exactly like the Pokemon do.
-// The user enters the name of a, a uh, *thing*, like they name of the creature, creature stats are generated from the name. The same name will generate the same stats if input more than once, it is consistent.
-// There will be some combat system of rival *yet to be named things* to compete
-// Basic stat
-// HP: your usual  hit points
-// Attack: how effective a hit is
-// Defense: mitigating a hit
-// Speed: determines order of move in battle
-
-// TBD: some kind of type modifier. If you want to get ridiculous you can use some AI to categorize the input and bin it into a type. Different advantages/disadvantages per type and type versus type.
-
-
 func main() {
 	fmt.Println("Giardiniera")
 }
-
-
